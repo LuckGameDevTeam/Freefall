66d02d960b4181923d3558be238bb1b0be3d9952
luckynelson2004 <luckynelson2004@gmail.com>
2014-10-21 14:14:45 +0800

    Add comment on game controller
    Add comment on game controller

--- Assets/Scripts/Level/GameController.cs
+++ Assets/Scripts/Level/GameController.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using Soomla.Store;
 
+/// <summary>
+/// Game controller.
+/// </summary>
 [RequireComponent (typeof(ObjectPool))]
 public class GameController : MonoBehaviour 
 {
