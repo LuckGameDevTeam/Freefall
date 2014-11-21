dd842ace6932565f520128ad3791d6ea4b8a8346
luckynelson2004 <luckynelson2004@gmail.com>
2014-11-21 10:40:09 +0800

    2014/11/21 - Limit Facebook post to get life duration

---
+++ Assets/Scripts/MetaData/FBPostTime.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class FBPostTime : PersistantMetaData 
+{
+	//time of fb post to wall
+	public string postTime;
+
+	/// <summary>
+	/// Load this instance.
+	/// </summary>
+	public static FBPostTime Load()
+	{
+		if(!SaveLoadManager.SharedManager.IsFileExist<FBPostTime>())
+		{
+			//create new one
+			FBPostTime newFile = new FBPostTime();
+			
+			newFile.postTime = "";
+			
+			SaveLoadManager.SharedManager.Save(newFile);
+			
+			return newFile;
+		}
+		
+		return SaveLoadManager.SharedManager.Load<FBPostTime> ();
+	}
+	
+	/// <summary>
+	/// Save data.
+	/// </summary>
+	/// <param name="data">Data.</param>
+	public static bool Save(FBPostTime data)
+	{
+		return SaveLoadManager.SharedManager.Save (data);
+		
+		
+	}
+}
