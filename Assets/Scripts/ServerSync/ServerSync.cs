1e48708fdd8d28c506d57bcdbca5e495c1a539a0
luckynelson2004 <luckynelson2004@gmail.com>
2014-12-04 18:09:31 +0800

    2014/12/4 - modify ServerSync

--- Assets/Scripts/ServerSync/ServerSync.cs
+++ Assets/Scripts/ServerSync/ServerSync.cs
@@ -19,11 +19,26 @@ public class ServerSync :MonoBehaviour
 
 	const int serverFatalError = -2;
 
+	const string authName = "test01";
+	const string authPass = "test01";
+
 	public delegate void OnInternetNotAvailable(ServerSync syncControl);
 	/// <summary>
 	/// Event when internet not available.
 	/// </summary>
 	public OnInternetNotAvailable Evt_OnInternetNotAvailable;
+
+	public delegate void OnAuthrizeSuccess(ServerSync syncControl);
+	/// <summary>
+	///  Event when authrize success.
+	/// </summary>
+	public OnAuthrizeSuccess Evt_OnAuthrizeSuccess;
+
+	public delegate void OnAuthrizeFail(ServerSync syncControl, int errorCode);
+	/// <summary>
+	/// Event when authrize fail.
+	/// </summary>
+	public OnAuthrizeFail Evt_OnAuthrizeFail;
 
 	public delegate void OnCreateAccountSuccess(ServerSync syncControl, string username, string password);
 	/// <summary>
@@ -88,6 +103,8 @@ public class ServerSync :MonoBehaviour
 	/// </summary>
 	private bool isLogin = false;
 
+	public bool IsLogedin{ get{ return isLogin; }}
+
 	/// <summary>
 	/// Is creating account or not.
 	/// </summary>
@@ -97,6 +114,16 @@ public class ServerSync :MonoBehaviour
 	/// Is logging into server or not.
 	/// </summary>
 	private bool isLoggingServer = false;
+
+	/// <summary>
+	/// Is authrizing or not.
+	/// </summary>
+	private bool isAuthrizing = false;
+
+	/// <summary>
+	/// The authrized.
+	/// </summary>
+	private bool authrized = false;
 
 	/// <summary>
 	/// The username that used to login server.
@@ -191,6 +218,91 @@ public class ServerSync :MonoBehaviour
 		
 		return retStr;
 	}
+
+	#region Authrization
+	public void Auth()
+	{
+		if(isAuthrizing)
+		{
+			return;
+		}
+
+		StartCoroutine ("DoAuth");
+	}
+
+	IEnumerator DoAuth()
+	{
+		isAuthrizing = true;
+
+		WWW wGo = new WWW (addrLoginServer);
+
+		yield return wGo;
+
+		isAuthrizing = false;
+
+		//if there is an error while download data
+		if(!string.IsNullOrEmpty(wGo.error))
+		{
+			Debug.LogError(gameObject.name+" "+wGo.error);
+			
+			if(Evt_OnAuthrizeFail != null)
+			{
+				Evt_OnAuthrizeFail(this, internalError);
+			}
+		}
+		else
+		{
+			if(string.IsNullOrEmpty(wGo.text))
+			{
+				if(Evt_OnAuthrizeFail != null)
+				{
+					Evt_OnAuthrizeFail(this, serverFatalError);
+				}
+			}
+			else
+			{
+				//parse json data
+				JSONNode data = JSON.Parse(wGo.text);
+				
+				
+				if((data == null) || (data == ""))
+				{
+					if(Evt_OnAuthrizeFail != null)
+					{
+						Evt_OnAuthrizeFail(this, serverFatalError);
+					}
+					
+				}
+				else
+				{
+					int error = int.Parse(data["error"].Value);
+					
+					//if data has an error
+					if(error != 0)
+					{
+						Debug.LogError(gameObject.name+" return data has error:"+error);
+						if(Evt_OnAuthrizeFail != null)
+						{
+							Evt_OnAuthrizeFail(this, error);
+						}
+					}
+					else
+					{
+						authrized = true;
+
+						uid = data["uid"];
+
+						
+						if(Evt_OnAuthrizeSuccess != null)
+						{
+							Evt_OnAuthrizeSuccess(this);
+						}
+					}
+				}
+			}
+		}
+	}
+	#endregion Authrization
 	
 	#region CreateAccount
 	/// <summary>
@@ -198,6 +310,7 @@ public class ServerSync :MonoBehaviour
 	/// </summary>
 	public void CreateAccount()
 	{
+
 		if(!IsInternetAvailable())
 		{
 			return;
@@ -211,8 +324,6 @@ public class ServerSync :MonoBehaviour
 		{
 			StartCoroutine("DoCreateAccount");
 		}
-
-
 	}
 
 	/// <summary>
@@ -224,7 +335,11 @@ public class ServerSync :MonoBehaviour
 	{
 		isCreatingAccount = true;
 
-		WWW wGo = new WWW (addrCreateAccount);
+		WWWForm postData = new WWWForm ();
+		postData.AddField ("device_id", SystemInfo.deviceUniqueIdentifier);
+		postData.AddField ("uid", uid);
+
+		WWW wGo = new WWW (addrCreateAccount, postData);
 
 		yield return wGo;
 
@@ -242,42 +357,52 @@ public class ServerSync :MonoBehaviour
 		}
 		else
 		{
-			//parse json data
-			JSONNode data = JSON.Parse(wGo.text);
-
-
-			if((data == null) || (data == ""))
+			if(string.IsNullOrEmpty(wGo.text))
 			{
 				if(Evt_OnCreateAccountFail != null)
 				{
 					Evt_OnCreateAccountFail(this, serverFatalError);
 				}
-
 			}
 			else
 			{
-				int error = int.Parse(data["error"].Value);
+				//parse json data
+				JSONNode data = JSON.Parse(wGo.text);
 				
-				//if data has an error
-				if(error != 0)
+				
+				if((data == null) || (data == ""))
 				{
-					Debug.LogError(gameObject.name+" return data has error:"+error);
 					if(Evt_OnCreateAccountFail != null)
 					{
-						Evt_OnCreateAccountFail(this, error);
+						Evt_OnCreateAccountFail(this, serverFatalError);
 					}
+					
 				}
 				else
 				{
-					//store username and password
-					username = data["name"].Value;
-					password = data["password"].Value;
+					int error = int.Parse(data["error"].Value);
 					
-					Debug.Log("Server return username:"+username+" password:"+password);
-					
-					if(Evt_OnCreateAccountSuccess != null)
+					//if data has an error
+					if(error != 0)
 					{
-						Evt_OnCreateAccountSuccess(this, username, password);
+						Debug.LogError(gameObject.name+" return data has error:"+error);
+						if(Evt_OnCreateAccountFail != null)
+						{
+							Evt_OnCreateAccountFail(this, error);
+						}
+					}
+					else
+					{
+						//store username and password
+						username = data["name"].Value;
+						password = data["password"].Value;
+						
+						Debug.Log("Server return username:"+username+" password:"+password);
+						
+						if(Evt_OnCreateAccountSuccess != null)
+						{
+							Evt_OnCreateAccountSuccess(this, username, password);
+						}
 					}
 				}
 			}
@@ -326,6 +451,7 @@ public class ServerSync :MonoBehaviour
 		WWWForm postData = new WWWForm ();
 		postData.AddField ("name", tmpLoginUserName);
 		postData.AddField ("password", encryptPassword);
+		postData.AddField ("device_id", SystemInfo.deviceUniqueIdentifier);
 
 
 		WWW wGo = new WWW (addrLoginServer, postData);
@@ -346,10 +472,7 @@ public class ServerSync :MonoBehaviour
 		}
 		else
 		{
-			//parse json data
-			JSONNode data = JSON.Parse(wGo.text);
-
-			if((data == null) || (data == ""))
+			if(string.IsNullOrEmpty(wGo.text))
 			{
 				if(Evt_OnLoginFail != null)
 				{
@@ -358,28 +481,43 @@ public class ServerSync :MonoBehaviour
 			}
 			else
 			{
-				int error = int.Parse(data["error"].Value);
+				//parse json data
+				JSONNode data = JSON.Parse(wGo.text);
 				
-				//if data has an error
-				if(error != 0)
+				if((data == null) || (data == ""))
 				{
-					Debug.LogError(gameObject.name+" return data has error:"+error);
 					if(Evt_OnLoginFail != null)
 					{
-						Evt_OnLoginFail(this, error);
+						Evt_OnLoginFail(this, serverFatalError);
 					}
 				}
 				else
 				{
-					//store uid
-					uid = data["uid"].Value;
+					int error = int.Parse(data["error"].Value);
 					
-					username = tmpLoginUserName;
-					password = tmpLoginPassword;
-					
-					if(Evt_OnLoginSuccess != null)
+					//if data has an error
+					if(error != 0)
 					{
-						Evt_OnLoginSuccess(this, uid);
+						Debug.LogError(gameObject.name+" return data has error:"+error);
+						if(Evt_OnLoginFail != null)
+						{
+							Evt_OnLoginFail(this, error);
+						}
+					}
+					else
+					{
+						isLogin = true;
+
+						//store uid
+						uid = data["uid"].Value;
+						
+						username = tmpLoginUserName;
+						password = tmpLoginPassword;
+						
+						if(Evt_OnLoginSuccess != null)
+						{
+							Evt_OnLoginSuccess(this, uid);
+						}
 					}
 				}
 			}
@@ -389,5 +527,11 @@ public class ServerSync :MonoBehaviour
 	}
 	#endregion Loggin server
 
+	#region Logout server
+	public void Logout()
+	{
+		isLogin = false;
+	}
+	#endregion Logout server
 
 }
