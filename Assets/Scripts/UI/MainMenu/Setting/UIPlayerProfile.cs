34825b5868c1820bd70fd12da09f36c0c6fe0f5c
luckynelson2004 <luckynelson2004@gmail.com>
2014-12-10 18:02:48 +0800

    2014/12/10 - Add player profile, change password not finished

---
+++ Assets/Scripts/UI/MainMenu/Setting/UIPlayerProfile.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIPlayerProfile : MonoBehaviour 
+{
+	public string errorKey = "Error";
+	public string enterNewPasswordKey = "EnterNewPassword";
+	public string changePasswordFailKey = "ChangePasswordFail";
+
+	public delegate void OnPlayerProfileClose(UIPlayerProfile profiel);
+	/// <summary>
+	/// Event when player profile close.
+	/// </summary>
+	public OnPlayerProfileClose Evt_OnPlayerProfileClose;
+
+	public UIInput accountInput;
+
+	public UIInput passwordInput;
+
+	/// <summary>
+	/// The new password input.
+	/// </summary>
+	public UIInput newPasswordInput;
+
+	/// <summary>
+	/// The alert control.
+	/// </summary>
+	public UIAlertControl alertControl;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void ShowPlayerProfile()
+	{
+		gameObject.SetActive (true);
+
+		UserProfile up = UserProfile.Load ();
+
+		accountInput.value = up.userName;
+		passwordInput.value = up.password;
+	}
+
+	public void ClosePlayerProfile()
+	{
+		if(Evt_OnPlayerProfileClose != null)
+		{
+			Evt_OnPlayerProfileClose(this);
+		}
+
+		gameObject.SetActive (false);
+	}
+
+	public void ChangePassword()
+	{
+		if(!string.IsNullOrEmpty(newPasswordInput.value))
+		{
+			//todo change password
+		}
+		else
+		{
+			alertControl.ShowAlertWindow(errorKey, enterNewPasswordKey);
+		}
+	}
+}
--- Assets/Scripts/UI/MainMenu/Setting/UISettingControl.cs
+++ Assets/Scripts/UI/MainMenu/Setting/UISettingControl.cs
@@ -4,12 +4,25 @@ using System.Collections;
 public class UISettingControl : MonoBehaviour 
 {
 	public delegate void EventOnSettingClose(UISettingControl control);
+	/// <summary>
+	/// The evt_ on setting close.
+	/// </summary>
 	public EventOnSettingClose Evt_OnSettingClose;
+
+	public GameObject settingMenu;
+
+	public UIPlayerProfile playerProfile;
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		settingMenu.SetActive (true);
+		playerProfile.gameObject.SetActive (false);
+	}
+
+	void OnEnable()
+	{
+		playerProfile.Evt_OnPlayerProfileClose += OnPlayerProfileClose;
 	}
 	
 	// Update is called once per frame
@@ -21,15 +34,32 @@ public class UISettingControl : MonoBehaviour
 	public void ShowSetting()
 	{
 		gameObject.SetActive (true);
+
+		settingMenu.SetActive (true);
+		playerProfile.gameObject.SetActive (false);
 	}
 
 	public void CloseSetting()
 	{
+		playerProfile.gameObject.SetActive (false);
+
 		gameObject.SetActive (false);
 
 		if(Evt_OnSettingClose != null)
 		{
 			Evt_OnSettingClose(this);
 		}
+	}
+
+	public void ShowPlayerProfile()
+	{
+		settingMenu.SetActive (false);
+
+		playerProfile.GetComponent<UIPlayerProfile> ().ShowPlayerProfile ();
+	}
+
+	void OnPlayerProfileClose(UIPlayerProfile profile)
+	{
+		settingMenu.SetActive (true);
 	}
 }
